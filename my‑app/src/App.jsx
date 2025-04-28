fc6d1250d6fee36fae39a1565b0f73f8ccd906ab
quantitychar <olenabogoslavska7@gmail.com>
2025-04-28 22:39:27 +0100

    feat: add module Button

--- my‑app/src/App.jsx
+++ my‑app/src/App.jsx
@@ -25,4 +25,3 @@ function App() {
 }
 
 export default App;
-PabxVsjehfhp3pph
---
+++ my‑app/src/assets/components/Button/Button.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import cn from "classnames";
+import PropTypes from "prop-types";
+import "./Button.scss";
+
+export default function Button({ children, onClick }) {
+  const {
+    onClick,
+    type = "button",
+    className,
+    href,
+    children,
+    to,
+    isOutline,
+    ...restProps
+  } = props;
+
+  const Element = href ? "a" : "button";
+
+  return (
+    <Element
+      type={!href && type}
+      className={cn("button", className, { outline: isOutline })}
+      onClick={onClick}
+      href={href}
+      {...restProps}
+    >
+      {children}
+    </Element>
+  );
+}
+
+Button.propTypes = {
+  onClick: PropTypes.func,
+  type: PropTypes.string,
+  className: PropTypes.string,
+  href: PropTypes.string,
+  children: PropTypes.any,
+  isOutline: PropTypes.bool,
+  restProps: PropTypes.object,
+};
