8f9b1968b4009ea219e25aec0ec982923fa144fc
quantitychar <olenabogoslavska7@gmail.com>
2025-05-02 22:36:29 +0100

    feat: added FavoritePage code

---
+++ my‑app/src/assets/pages/FavoritePage/FavoritePage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./FavoritePage.scss";
+import ModalMovies from "@/composition/ModalMovies/ModalMovies";
+import PosterCard from "@/components/PosterCard/PosterCard";
+
+const FavoritePage = ({ data = [], onFavorite }) => {
+  return (
+    <>
+      <div className="posters-wrapper">
+        {data.map((item) => (
+          <PosterCard key={item.id} poster={item} />
+        ))}
+      </div>
+      {/* <ModalMovies /> */}
+    </>
+  );
+};
+
+export default FavoritePage;
